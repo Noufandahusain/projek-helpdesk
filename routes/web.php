64ff0072523922dd3197c1c3fbe764e8291f97a7
conaryy <anggamajid185@gmail.com>
2025-12-02 02:49:23 +0700

    fixing typo

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::redirect('/', '/login');
-
 Route::get('/', function () {
     if (auth()->check()) {
         return auth()->user()->isAdmin()
